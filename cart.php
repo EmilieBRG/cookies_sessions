f39b838324b61ae63f11148e7c6a8d04a4b93cd1
EmilieBRG <emibgr@gmail.com>
2018-07-12 21:06:03 +0200

    c'est bon les cookies!

---
+++ cart.php
@@ -0,0 +1,23 @@
+<?php
+require 'inc/head.php';
+session_start();
+
+
+if (!isset($_SESSION['loginname'])) {
+    header('Location: login.php');
+}
+?>
+<section class="cookies container-fluid">
+    <div class="row">
+        <?php
+        if (isset($_COOKIE['panier'])){
+            $cart = substr($_COOKIE['panier'],0,strlen($_COOKIE['panier'])-1);
+            $data=explode(',',$cart) ;
+            foreach($data as $id){
+                echo '<div class="col-xs-6">Article ' . $cookies[$id] . ' commandé</div>';
+            }
+        }
+        ?>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
